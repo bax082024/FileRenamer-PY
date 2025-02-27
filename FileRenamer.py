ad1ad983b92218e7cd1d25f905650b804ce4e78d
bax082024 <bax082024@gmail.com>
2025-02-27 08:49:08 +0100

    basic renaming script

--- FileRenamer.py
+++ FileRenamer.py
@@ -4,4 +4,13 @@ folder_path = r"C:\Users\busch\OneDrive\Documents\aagergaergaerg\PYYYTest"
 
 files = os.listdir(folder_path)
 
+for index, file in enumerate(files):
+    old_path = os.path.join(folder_path, file)
+    new_name = f"file_{index+1}.txt"
+    new_path = os.path.join(folder_path, new_name)
 
+    os.rename(old_path, new_path)
+    print(f"Renamed: {file} -> {new_name}")
+
+
+print("All files renamed successfully !")
